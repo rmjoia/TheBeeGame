56650c763d534caaf1e1b871c4aac58ffcf4fab0
Ricardo Joia <ricardo.joia@live.com.pt>
2016-01-28 23:12:19 +0000

    refactor
    add override to game over when Queen is Killed

--- TheBeeGame/Models/Queen.cs
+++ TheBeeGame/Models/Queen.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using TheBeeGame.Interfaces;
 
 namespace TheBeeGame.Models
 {
     public class Queen : Bee
     {
-
         public Queen(IBee bee) : base(bee)
         {
 
+        }
+
+        public override IList<IBee> CheckStatus(IBee bee, IList<IBee> hive)
+        {
+            if (bee.LifeSpan == 0)
+            {
+                hive = new List<IBee>();
+            }
+
+            return hive;
         }
     }
 }
